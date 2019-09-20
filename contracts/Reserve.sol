7ad1046567c00a8ccc172c564317beea6d3c9cc5
Taylor Brent <taylor.w.brent@gmail.com>
2019-09-20 16:50:41 -0700

    use openzeppelin IERC20 instead of in-file

--- contracts/Reserve.sol
+++ contracts/Reserve.sol
@@ -1,24 +1,9 @@
 pragma solidity ^0.5.8;
 
+import "./zeppelin/token/ERC20/IERC20.sol";
 import "./zeppelin/math/SafeMath.sol";
 import "./Ownable.sol";
 import "./ReserveEternalStorage.sol";
-
-/**
- * @title The standard ERC20 interface
- * @dev see https://eips.ethereum.org/EIPS/eip-20
- */
-interface IERC20 {
-    function transfer(address, uint256) external returns (bool);
-    function approve(address, uint256) external returns (bool);
-    function transferFrom(address, address, uint256) external returns (bool);
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address) external view returns (uint256);
-    function allowance(address, address) external view returns (uint256);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed holder, address indexed spender, uint256 value);
-}
-
 
 /**
  * @title An interface representing a contract that calculates transaction fees
