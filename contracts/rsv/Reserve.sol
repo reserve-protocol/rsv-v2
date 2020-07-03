b88f6f34aece43e99cc9783f6767316cfaa2311c
Taylor Brent <taylor.w.brent@gmail.com>
2020-07-02 19:55:14 -0700

    Revert "Change rsv constructor to set eternal storage"

--- contracts/rsv/Reserve.sol
+++ contracts/rsv/Reserve.sol
@@ -70,7 +70,7 @@ contract Reserve is IERC20, Ownable {
     uint8 public constant decimals = 18;
 
     /// Initialize critical fields.
-    constructor(address eternalStorageAddr) public {
+    constructor() public {
         pauser = msg.sender;
         feeRecipient = msg.sender;
         // minter defaults to the zero address.
@@ -80,7 +80,8 @@ contract Reserve is IERC20, Ownable {
 
         trustedTxFee = ITXFee(address(0));
         trustedRelayer = address(0);
-        trustedData = ReserveEternalStorage(eternalStorageAddr);
+        trustedData = new ReserveEternalStorage();
+        trustedData.nominateNewOwner(msg.sender);
     }
 
     /// Accessor for eternal storage contract address.
